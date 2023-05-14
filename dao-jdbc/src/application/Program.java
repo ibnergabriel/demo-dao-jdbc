e26454747cfd1e45e81dffc68d078ffbe3231196
ibnergabriel <ibner68@gmail.com>
2023-05-14 13:56:38 -0300

    Seller entity class

--- dao-jdbc/src/application/Program.java
+++ dao-jdbc/src/application/Program.java
@@ -1,12 +1,24 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department dp = new Department(1,"Books");
-        System.out.println(dp);
+
+        Seller seller = new Seller
+                (21,
+                        "Davi",
+                        "davijudo22@gmail.com",
+                        new Date(),
+                        2100.00,
+                        new Department(99,"GPU"));
+
+        System.out.println(seller);
     }
 }
