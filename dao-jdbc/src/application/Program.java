4e0fdd7fe5a42471e76d0db6fe94e292ba7fa273
ibnergabriel <ibner68@gmail.com>
2023-05-17 19:18:42 -0300

    insert implementation

--- dao-jdbc/src/application/Program.java
+++ dao-jdbc/src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -31,5 +32,11 @@ public class Program {
         for(Seller obj: list){
             System.out.println(obj);
         }
+
+        System.out.println("\n===== TEST 4: seller insert ======");
+        Seller newSeller = new Seller(null, "RonaldoFenomeno", "r9@gmail.com", new Date(),10000.0,
+                department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New ID = " + newSeller.getId());
     }
 }
